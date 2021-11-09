7cf4d5943927de0f501e141add74da2aad702ab1
Anton689 <zizugum@gmail.com>
2021-11-09 12:44:45 +0300

    added solution Shortest Word

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -35,3 +35,17 @@ function getSum( a,b ){
 	const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
 	return array.map((x) => x % 2 ? odd.shift() : x);
  };    
+
+ //Simple, given a string of words, return the length of the shortest word(s).
+//String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+	let res = s.split(' ');
+	let a = Infinity;
+	
+	for (let i = 0; i < res.length; i++) {
+	  a = Math.min(res[i].length, a);
+	}
+	return a;
+ }
+ 
