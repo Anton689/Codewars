ded5deb9f2d8f9ac65f27e93b4e68fb72873aa6f
Anton689 <zizugum@gmail.com>
2021-11-09 11:41:26 +0300

    added solution Beginner Series #3 Sum of Numbers

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -14,3 +14,17 @@ function digPow(n, p){
 	return (''+arr).includes('.')?-1:arr
 }
 	  
+//Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+//Note: a and b are not ordered!
+
+function getSum( a,b ){
+
+	if(a == b) {
+		 return a
+	}
+	else if (a < b) {
+		 return a + getSum(a+1, b)
+	} else {
+		 return a + getSum(a-1, b)
+	};
+ }
