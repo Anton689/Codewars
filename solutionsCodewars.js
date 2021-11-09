45612f8542d2d05094a6ec90f0a6f8e41de2d964
Anton689 <zizugum@gmail.com>
2021-11-09 11:42:42 +0300

    added solution Sort the odd

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -28,3 +28,10 @@ function getSum( a,b ){
 		 return a + getSum(a-1, b)
 	};
  }
+
+ //You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+ function sortArray(array) {
+	const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+	return array.map((x) => x % 2 ? odd.shift() : x);
+ };    
