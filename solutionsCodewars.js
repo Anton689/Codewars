196596afa385f2b245362dc09d5d4273454fd0b4
Anton689 <zizugum@gmail.com>
2021-11-09 11:39:58 +0300

    added solution Playing with digits

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -6,4 +6,11 @@ function descendingOrder(n){
 	return parseInt(String(n).split('').sort().reverse().join(''))
 	  }
 
+//Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer pwe want to find a positive integer k, if it exists, such as the sum of the digits of n taken to the successive powers of p is equal to k * n.
+//In other 
+
+function digPow(n, p){
+	let arr=n.toString().split('').map(Number).reduce((acc,cur,i,arr)=>acc+(Math.pow(arr[i],p+i)),0)/n
+	return (''+arr).includes('.')?-1:arr
+}
 	  
