de007460456e85c9a78c8d59d4e3326bb48e0ae8
Anton689 <zizugum@gmail.com>
2021-11-11 18:12:05 +0300

    added solution Count the number of divisors of a positive integer n.

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -95,3 +95,17 @@ function validatePIN (pin) {
 function stringToArray(string){
 	return string.split (' ');
  }
+
+ //Count the number of divisors of a positive integer n.
+
+ function getDivisorsCnt(n){
+	if (n < 1) {
+	  return n;
+	}
+	let count = 0;
+	for (let i = 1; i <= n; i++) {
+	  if(n % i == 0)
+	  count++;
+	}
+	return count;
+}
