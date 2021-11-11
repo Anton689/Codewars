6f24e4e315466977b147eeff0399802de6482847
Anton689 <zizugum@gmail.com>
2021-11-11 19:11:34 +0300

    find mistake solution

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -121,3 +121,11 @@ var isSquare = function(n){
 	}
 	return false;
  }
+
+ //Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+ function greet(name){
+	if(name === "Johnny")
+	  return "Hello, my love!"
+	return "Hello, " + name + "!";
+	}
