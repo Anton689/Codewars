4ae54cdb0fc746b8f1f3bf39f25e4442c89d26ba
Anton689 <zizugum@gmail.com>
2021-11-11 19:25:26 +0300

    added kata solution

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -129,3 +129,13 @@ var isSquare = function(n){
 	  return "Hello, my love!"
 	return "Hello, " + name + "!";
 	}
+
+	//Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+	function findSum(n) {
+		let sum = 0;
+		for(let i = 1; i <= n; i++){
+		  if(i % 3 == 0 || i % 5 == 0)sum+=i;
+		}
+		return sum;
+	 }
