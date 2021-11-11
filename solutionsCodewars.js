ba0fa08bddf1ea1c89acbb3bd8c775950eac056b
Anton689 <zizugum@gmail.com>
2021-11-11 17:37:24 +0300

    added new kata solution

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -89,3 +89,9 @@ function validatePIN (pin) {
  }
  return true;
 }
+
+//Write a function to split a string and convert it into an array of words. For example:"Robin Singh" ==> ["Robin", "Singh"]
+
+function stringToArray(string){
+	return string.split (' ');
+ }
