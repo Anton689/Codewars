34d150dc52902b0a81559894c5457be77e6b5d87
Anton689 <zizugum@gmail.com>
2021-11-11 19:33:23 +0300

    Complete the method that takes a boolean

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -139,3 +139,9 @@ var isSquare = function(n){
 		}
 		return sum;
 	 }
+
+	 //Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+	 function boolToWord( bool ){
+		return bool === true ? 'Yes' : 'No';
+	 }
