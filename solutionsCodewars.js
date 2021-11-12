f5f9523adc6005943479deda2c46cf0e15c0c29f
Anton689 <zizugum@gmail.com>
2021-11-12 16:42:12 +0300

    Define a function that removes duplicates from an array of numbers and returns it as a result.

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -157,3 +157,9 @@ var isSquare = function(n){
 		return String.fromCharCode(c);
 	 }
 
+	 //Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+	 function distinct(a) {
+		return Array.from(new Set(a));
+	}
+
