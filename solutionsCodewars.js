ecc1690f0ad7fb935a3942af55d3cae39b650952
Anton689 <zizugum@gmail.com>
2021-11-12 17:04:15 +0300

    This time no story, no theory. The examples below show you how to write function accum:

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -162,4 +162,20 @@ var isSquare = function(n){
 	 function distinct(a) {
 		return Array.from(new Set(a));
 	}
+	//This time no story, no theory. The examples below show you how to write function accum:
+
+	function accum(s) {
+		const result = []
+		const lowerStr = s.toLowerCase()
+	 
+		for(let i = 0; i < lowerStr.length; i++) {
+		  let str = lowerStr[i].toUpperCase()
+		  for(let j = 0; j < i; j++) {
+			 str += lowerStr[i]
+		  }
+		  result.push(str)
+		}
+	 
+		return result.join("-")
+	 }
 
