24b1d5e98a6f3e7fb498a431b2e6d8898e66828f
Anton689 <zizugum@gmail.com>
2021-11-12 17:20:56 +0300

    In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -179,3 +179,10 @@ var isSquare = function(n){
 		return result.join("-")
 	 }
 
+	 //In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+	 function highAndLow(numbers){
+		numbers = numbers.split(' ').map(Number);
+		  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+		}
+
