25705fda27cfe11d72235ee0c73ecb38b5156e13
Anton689 <zizugum@gmail.com>
2021-11-13 15:06:16 +0300

    Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -203,6 +203,17 @@ var isSquare = function(n){
 		 String.prototype.toAlternatingCase = function () {
   			return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('');
 		 }
+
+		 //Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+		 
+		 function angle(n) {
+			if (n == 2){
+			  return n
+			}
+			let sum = 180 * (n - 2);
+			return sum;
+		 }
+
 		 
 
 
