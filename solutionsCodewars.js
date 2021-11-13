4c26829077504614a0ba1e2794a98f46dfc53bba
Anton689 <zizugum@gmail.com>
2021-11-13 14:53:50 +0300

    Define String.prototype.toAlternatingCase

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -198,4 +198,11 @@ var isSquare = function(n){
 			return n * (factorial(n-1))
 		 }
 
+		 //Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. 
+
+		 String.prototype.toAlternatingCase = function () {
+  			return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('');
+		 }
 		 
+
+
