621154e496d3845effdcd531305d15623a36452c
Anton689 <zizugum@gmail.com>
2021-11-14 14:39:49 +0300

    Print all numbers up to 3rd parameter which are multiple of both 1st and 2nd parameter.

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -267,6 +267,16 @@ var isSquare = function(n){
 			  .map(item => `${item} * ${number} = ${item*number}`)
 			  .join('\n');
 			}
+
+		//Print all numbers up to 3rd parameter which are multiple of both 1st and 2nd parameter.
+
+		function multiples(s1,s2,s3){
+			const arr = [];
+			for (let i = 1 ; i < s3; i++)
+			{ if (i % s1 === 0 && i % s2 === 0)arr.push(i)
+			}
+			return arr;
+		 }
 		 
 
 
