13ff0960d82852a38efa9f4abbc40f84e5a0a0c6
Anton689 <zizugum@gmail.com>
2021-11-14 15:00:38 +0300

    You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -289,7 +289,11 @@ var isSquare = function(n){
 			}
 			return false;
 		 }
-		 
 
 
+//You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+function reverse(string){
+	return string.split(' ').reverse().join(' ');
+ }
 
