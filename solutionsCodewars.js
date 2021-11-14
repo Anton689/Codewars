2048e7c1e383b7752f0fc8e25415eb6f6dc2199e
Anton689 <zizugum@gmail.com>
2021-11-14 15:55:51 +0300

    added solution

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -319,3 +319,12 @@ function getSumOfDigits(integer) {
    return sum;
 }
 
+//The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+
+function numberToPower(number, power){
+	let result = 1;
+	for (let i = 1; i <= power; i++){
+	  result = result * number;
+	}
+	return result;
+ }
