d5e6eaf0bb75d23e7b39720826cae892c75f276c
Anton689 <zizugum@gmail.com>
2021-11-15 16:01:02 +0300

    added kata solution

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -352,3 +352,13 @@ function numberToPower(number, power){
    return (a > b ? a % b : b % a)
 }
 
+//Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+
+function sumTriangularNumbers(n) {
+	let sum=0,total=0;
+	for(let i=0;i<=n;i++){
+	sum+=i*1
+	total+=sum
+	} return total
+ }
+
