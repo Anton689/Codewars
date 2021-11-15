f0f02d3677c0c04960bc525cf940952fca4805df
Anton689 <zizugum@gmail.com>
2021-11-15 16:40:42 +0300

    Calculate how many times a number can be divided by a given number.

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -407,3 +407,14 @@ function notVisibleCubes(n) {
  var a = "code";
 var b = "wa.rs";
 var name = a + b;
+
+//Calculate how many times a number can be divided by a given number.
+
+const divisions = (n, divisor) => {
+	let count=0;
+	while(n>1){
+	count++
+	n=n/divisor
+	}
+	return count-1;
+ };
