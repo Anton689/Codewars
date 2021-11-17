672337a6cb2cedc51852a99ae47cc791492e52b6
Anton689 <zizugum@gmail.com>
2021-11-17 20:26:17 +0300

    Check the exam

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -575,6 +575,20 @@ const getEvenNumbers = numbersArray =>
 	 }
 
 	 //
+//
+//Check the exam
+
+const checkExam = (answers, responses) => {
+	const score = responses.reduce((total, response, index) => {
+	  if (!response) return total
+	  if (response === answers[index]) return total + 4
+	  if (response !== answers[index]) return total - 1
+ 
+	  return total
+	}, 0)
+ 
+	return score > 0 ? score : 0
+ }
 
 
 
