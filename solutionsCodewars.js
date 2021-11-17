25880df90eb045e3318b556e118b02e74cfd25b6
Anton689 <zizugum@gmail.com>
2021-11-17 11:30:05 +0300

    Waiting room

--- solutionsCodewars.js
+++ solutionsCodewars.js
@@ -483,5 +483,15 @@ let greet = function(name) {
 	return `Hello ${(name.slice(0,1).toUpperCase())+(name.slice(1).toLowerCase())}!`
 	};
 
+//Waiting room
+
+function lastChair(n) {
+	return n - 1;
+ }
+ 
+ 
+
+
+
 
 
